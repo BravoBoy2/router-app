0d6684608096cf7cad1e74d956ef8035b4592b5a
abubakar <abubakar.siddik2146@gmail.com>
2024-05-13 01:08:16 +0100

    chore: Update styles and dependencies
    - Update styles.css to include global styles for html, body
    - Add CSS for .links class in app.component.css
    - Update angular.json to include @angular/material/prebuilt-themes/indigo-pink.css
    - Update index.html to include Google Fonts and Material Icons
    - Update app.config.ts to include provideAnimationsAsync()
    - Update app.routes.ts to include routes for FirstComponent, SecondComponent, NotFoundComponent, and ContactComponent
    - Update app.component.ts to import Router and ContactModule, and add goToContact() method
    - Update package.json to update dependencies and devDependencies
    
    ```

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { ContactModule } from './contact/contact.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router : Router) {
+    
+
+   }
   title = 'router-app';
+
+  goToContact() {
+  this.router.navigate(['contact']);
+  }
 }
